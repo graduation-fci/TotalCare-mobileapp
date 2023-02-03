15b449235ea3183eda4395254dc59963eb6f3589
hassan-prog <alielsayed1112@gmail.com>
2023-02-03 08:49:16 +0200

    added user model

---
+++ grad_login/lib/models/user.dart
@@ -0,0 +1,17 @@
+class User {
+  final String username;
+  final String firstName;
+  final String lastName;
+  final String email;
+  final String password;
+  final String profile_type;
+
+  User({
+    required this.username,
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+    required this.password,
+    required this.profile_type,
+  });
+}
