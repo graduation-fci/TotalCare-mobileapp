88a067d3da864a1ca4001433589c6414ea28ff3d
hassan-prog <alielsayed1112@gmail.com>
2023-02-23 22:30:33 +0200

    renamed file & used isRegister bool value to show register/login screen

--- grad_login/lib/screens/login_screen.dart
+++ grad_login/lib/screens/login_screen.dart
@@ -1,11 +1,15 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../providers/userService.dart';
+import '../widgets/input_field.dart';
 import '../providers/authService.dart';
-import '../models/user.dart';
+
+import 'register_screen.dart';
 
 class LoginScreen extends StatefulWidget {
+  static const String routeName = '/login';
   const LoginScreen({super.key});
 
   @override
@@ -13,60 +17,261 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  var newUser = User(
-    username: 'hassan003',
-    // id: 'hassan001',
-    firstName: 'Hassan',
-    lastName: 'Elsayed',
-    email: 'hassanleave03@test.com',
-    password: 'Aa012345543210',
-    profile_type: 'STD',
-    // birthdate: DateTime.now(),
-  );
+  final _formKey = GlobalKey<FormState>();
+  var isActive = false;
+  var visible = true;
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  String errorMessage = '';
+
+  bool isButtonActive() {
+    isActive =
+        nameController.text.isNotEmpty && passwordController.text.isNotEmpty
+            ? true
+            : false;
+    setState(() {
+      isActive;
+    });
+    return isActive;
+  }
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Login demo',
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-        ),
-        actions: [
-          TextButton(
-            onPressed: () async => {
-              // await Provider.of<Users>(context, listen: false)
-              //     .login('hassanelsayed009', 'test123123', context)
-              // await Provider.of<Users>(context, listen: false).register(
-              //   newUser,
-              //   context,
-              // )
-              await Provider.of<AuthService>(context, listen: false).login(
-                'hassan003',
-                'Aa012345543210',
-                context,
-              )
-              // await Provider.of<AuthService>(context, listen: false).refreshJwt(
-              //   context,
-              // )
-              // await Provider.of<AuthService>(context, listen: false).logout()
-            },
-            child: const Icon(
-              Icons.add,
-              color: Colors.white,
+    var mediaQuery = MediaQuery.of(context);
+    var mainTopPadding =
+        AppBar().preferredSize.height + mediaQuery.size.height * 0.07;
+    var errorMessage = Provider.of<AuthService>(context, listen: false).error;
+
+    return !Provider.of<AuthService>(context).isRegister
+        ? SafeArea(
+            child: GestureDetector(
+              onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+              child: Scaffold(
+                resizeToAvoidBottomInset: false,
+                body: Padding(
+                  padding: EdgeInsets.only(
+                    top: mainTopPadding,
+                    left: mediaQuery.size.width * 0.05,
+                    right: mediaQuery.size.width * 0.05,
+                  ),
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          child: Text(
+                            'Total Care',
+                            style: TextStyle(
+                              color: Theme.of(context).colorScheme.primary,
+                              fontWeight: FontWeight.w600,
+                              fontSize: 32,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.all(10),
+                          child: Text(
+                            "sign in",
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Theme.of(context).colorScheme.primary,
+                            ),
+                          ),
+                        ),
+                        InputField(
+                          nameController: nameController,
+                          labelText: 'Username',
+                          isPassword: false,
+                          prefixIcon: Icons.person,
+                        ),
+                        InputField(
+                          nameController: passwordController,
+                          labelText: 'Password',
+                          isPassword: true,
+                          prefixIcon: Icons.lock,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Container(
+                              alignment: Alignment.centerRight,
+                              child: TextButton(
+                                onPressed: () {},
+                                child: Text(
+                                  'Forget Password?',
+                                  style: TextStyle(
+                                    color: Theme.of(context)
+                                        .colorScheme
+                                        .primary
+                                        .withOpacity(0.9),
+                                    fontSize: 12,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor:
+                                Theme.of(context).colorScheme.secondary,
+                            fixedSize: Size(
+                              mediaQuery.size.width * 0.85,
+                              mediaQuery.size.height * 0.06,
+                            ),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(40),
+                            ),
+                          ),
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              try {
+                                Provider.of<AuthService>(context, listen: false)
+                                    .login(nameController.text,
+                                        passwordController.text, context);
+                                print(errorMessage);
+                              } catch (_) {
+                                // if (errorMessage!.isNotEmpty) {
+                                //   ScaffoldMessenger.of(context)
+                                //       .showSnackBar(SnackBar(
+                                //     content: Text(errorMessage),
+                                //   ));
+                                // }
+                              }
+                            }
+                          },
+                          // isButtonActive()
+                          //     ?
+                          //     : null,
+                          child: Text(
+                            'Login',
+                            style: TextStyle(
+                                color: Theme.of(context).colorScheme.primary),
+                          ),
+                        ),
+                        Column(
+                          children: [
+                            Row(
+                              children: [
+                                Expanded(
+                                  child: Container(
+                                    margin: EdgeInsets.only(
+                                        left: 15.0, right: 15.0),
+                                    child: Divider(
+                                      color: Colors.black,
+                                      height: 36,
+                                    ),
+                                  ),
+                                ),
+                                Text(
+                                  'or',
+                                ),
+                                Expanded(
+                                  child: Container(
+                                    margin: EdgeInsets.only(
+                                        left: 15.0, right: 15.0),
+                                    child: Divider(
+                                      color: Colors.black,
+                                      height: 36,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                print('tap');
+                              },
+                              style: TextButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(40),
+                                ),
+                                minimumSize: Size(
+                                  mediaQuery.size.width * 0.85,
+                                  mediaQuery.size.height * 0.06,
+                                ),
+                                side: BorderSide(
+                                  width: 0.8,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                              child: SizedBox(
+                                width: mediaQuery.size.width * 0.8,
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Image.asset(
+                                      'assets/images/google.png',
+                                      height: 20,
+                                      width: 20,
+                                    ),
+                                    SizedBox(
+                                      width: 10,
+                                    ),
+                                    Text(
+                                      'Sign in With Google',
+                                      style: TextStyle(
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .primary,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.only(top: 12),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    'Don\'t have an account? ',
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      color:
+                                          Theme.of(context).colorScheme.primary,
+                                    ),
+                                  ),
+                                  TextButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        Provider.of<AuthService>(context,
+                                                listen: false)
+                                            .isRegister = true;
+                                      });
+                                    },
+                                    child: Text(
+                                      'Sign up',
+                                      style: TextStyle(
+                                          color: Theme.of(context)
+                                              .colorScheme
+                                              .primary
+                                              .withOpacity(0.9),
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
             ),
           )
-        ],
-      ),
-      body: Container(
-        height: 300,
-        child: ListView.builder(
-          itemBuilder: (context, item) => Text(
-            newUser.username,
-          ),
-          itemCount: Provider.of<UserService>(context).users.length,
-        ),
-      ),
-    );
+        : RegisterFormScreen();
   }
 }
