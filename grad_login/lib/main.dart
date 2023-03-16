ccff1ea9a7ba78457338f34b0dd79ddd76c3232b
hassan-prog <alielsayed1112@gmail.com>
2023-03-16 16:21:40 +0200

    switched main screen to be signin_screen

--- grad_login/lib/main.dart
+++ grad_login/lib/main.dart
@@ -1,13 +1,11 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
-import 'package:grad_login/infrastructure/exam/ordering.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import '../screens/login_screen.dart';
 import '../screens/register_screen.dart';
 import '../screens/exams_screen.dart';
+import '../screens/signin_screen.dart';
 import 'providers/examProvider.dart';
 import 'providers/userProvider.dart';
 import 'providers/authProvider.dart';
@@ -42,8 +40,9 @@ class MyApp extends StatelessWidget {
           primary: const Color.fromARGB(255, 22, 36, 66),
           secondary: const Color.fromRGBO(167, 252, 132, 0.7),
         )),
-        home: const LoginScreen(),
+        home: const SignInScreen(),
         routes: {
+          SignInScreen.routeName: (ctx) => const SignInScreen(),
           LoginScreen.routeName: (ctx) => const LoginScreen(),
           RegisterFormScreen.routeName: (ctx) => const RegisterFormScreen(),
           ExamsScreen.routeName: (ctx) => const ExamsScreen(),
