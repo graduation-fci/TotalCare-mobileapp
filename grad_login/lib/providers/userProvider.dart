beb9e2cf5086610ff200a3be0059ba3119184001
hassan-prog <alielsayed1112@gmail.com>
2023-03-17 15:10:57 +0200

    created handleOrder function for asc and desc order

--- grad_login/lib/providers/userProvider.dart
+++ grad_login/lib/providers/userProvider.dart
@@ -2,9 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:grad_login/infrastructure/user/userService.dart';
-import 'package:grad_login/providers/examProvider.dart';
 import 'package:http/http.dart' as http;
-import 'package:provider/provider.dart';
 
 import '../models/user.dart';
 import '../my_config.dart';
@@ -32,45 +30,10 @@ class UserProvider with ChangeNotifier {
   }
 
   Future<void> getSearchedData(String searchQuery) {
-    return userService.getSearchedData(searchQuery);
+    return userService.fetchData(search: searchQuery);
   }
 
-  Future<void> ascendingOrder() {
-    return userService.ascendingOrder();
+  Future<void> handleOrder(String searchQuery, {ordering}) {
+    return userService.fetchData(search: searchQuery, ordering: ordering);
   }
-
-  Future<void> descendingOrder() {
-    return userService.descendingOrder();
-  }
-
-  // Future<void> fetchUsers(BuildContext context) async {
-  //   final jsonString = await DefaultAssetBundle.of(context)
-  //       .loadString('assets/my_config.json');
-  //   final dynamic jsonMap = jsonDecode(jsonString)['apiUrl'];
-  //   final apiUrl = Uri.https(jsonMap, '/users');
-  //   try {
-  //     final response = await http.get(apiUrl);
-  //     final fetchedData = json.decode(response.body) as Map<String, dynamic>;
-  //     final List<User> loadedData = [];
-
-  //     fetchedData.forEach((userId, userData) {
-  //       loadedData.add(
-  //         User(
-  //           // id: userId,
-  //           // birthdate: userData['birthdate'],
-  //           username: userData['username'],
-  //           firstName: userData['firstName'],
-  //           lastName: userData['lastName'],
-  //           email: userData['email'],
-  //           password: userData['password'],
-  //           profile_type: userData['rePassword'],
-  //         ),
-  //       );
-  //     });
-  //     _users = loadedData;
-  //     notifyListeners();
-  //   } catch (error) {
-  //     throw error;
-  //   }
-  // }
 }
