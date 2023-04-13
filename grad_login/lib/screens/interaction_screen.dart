baba768eb1ba2edc06b94276070ad9db2cc4ae71
hassan-prog <alielsayed1112@gmail.com>
2023-04-13 05:49:32 +0200

    replaced old interaction screen with new

--- grad_login/lib/screens/interaction_screen.dart
+++ grad_login/lib/screens/interaction_screen.dart
@@ -1,15 +1,142 @@
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatefulWidget {
-  const MyWidget({super.key});
+class InteractionScreen extends StatefulWidget {
+  static const routeName = '/interaction-screen';
+  const InteractionScreen({super.key});
 
   @override
-  State<MyWidget> createState() => _MyWidgetState();
+  State<InteractionScreen> createState() => _InteractionScreenState();
 }
 
-class _MyWidgetState extends State<MyWidget> {
+class _InteractionScreenState extends State<InteractionScreen> {
+  final TextEditingController searchController = TextEditingController();
+  final TextEditingController searchController2 = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: GestureDetector(
+        onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+        child: Scaffold(
+          resizeToAvoidBottomInset: false,
+          backgroundColor: Colors.grey.shade200,
+          appBar: AppBar(
+              title: const Text(
+            'Interactions',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+          )),
+          body: Container(
+            padding: EdgeInsets.all(20),
+            color: Colors.grey.shade200,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        controller: searchController,
+                        decoration: InputDecoration(
+                          hintText: 'Enter a drug name ',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 8,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Add'),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'unsaved interactions list',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        )),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Start over',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    Expanded(
+                      child: TextFormField(
+                        controller: searchController2,
+                        decoration: InputDecoration(
+                          hintText: 'Drug 1 (example)',
+                          border: OutlineInputBorder(),
+                          suffixIcon: IconButton(
+                            icon: Icon(Icons.clear),
+                            onPressed: () {
+                              searchController2.clear();
+                            },
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Check interactions',
+                          // style: ElevatedButton.styleFrom(backgroundColor: Colors.greenAccent),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Save',
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
