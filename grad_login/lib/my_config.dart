5924949a7af0e405c7b51e74b8655a93ce1f8836
hassan-prog <alielsayed1112@gmail.com>
2023-04-13 08:51:54 +0200

    applied new api endpoint

--- grad_login/lib/my_config.dart
+++ grad_login/lib/my_config.dart
@@ -1,6 +1,6 @@
 class Config {
-  static const String apiUrl = "http://192.168.1.3:8000";
-  static const String exams = '$apiUrl/exam/exams/';
+  static const String apiUrl = "http://192.168.1.8:8001";
+  static const String simpleMeds = '$apiUrl/medicine/simple_meds/';
   static const String login = '$apiUrl/auth/jwt/create/';
   static const String register = '$apiUrl/auth/users/';
 }
