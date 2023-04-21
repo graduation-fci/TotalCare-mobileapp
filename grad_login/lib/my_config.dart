8c4eea3d849d3971c9db407494dfb346bcd95386
hassan-prog <alielsayed1112@gmail.com>
2023-04-21 23:38:14 +0200

    modified login endpont to the right one

--- grad_login/lib/my_config.dart
+++ grad_login/lib/my_config.dart
@@ -1,6 +1,7 @@
 class Config {
   static const String apiUrl = "http://192.168.1.8:8001";
   static const String simpleMeds = '$apiUrl/medicine/simple_meds/';
+  static const String interactionMain = '$apiUrl/medicine/interactions/';
   static const String login = '$apiUrl/auth/jwt/create/';
   static const String register = '$apiUrl/auth/users/';
 }
