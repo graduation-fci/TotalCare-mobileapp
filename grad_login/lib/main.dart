f3fc28d8ee7e0b0c116d51cefe7054bc27017ccf
hassan-prog <alielsayed1112@gmail.com>
2023-04-21 23:46:36 +0200

    added InteractionsProvider to the list of providers & added new route

--- grad_login/lib/main.dart
+++ grad_login/lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:grad_login/screens/tabs_screen.dart';
+import 'package:grad_login/providers/interactionsProvider.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
+import '../screens/show_interactions_results_screen.dart';
 import '../screens/login_screen.dart';
 import '../screens/register_screen.dart';
 import '../screens/exams_screen.dart';
@@ -31,19 +32,25 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider.value(
           value: MedicineProvider(),
         ),
+        ChangeNotifierProvider.value(
+          value: InteractionsProvider(),
+        ),
       ],
       child: MaterialApp(
         localizationsDelegates: AppLocalizations.localizationsDelegates,
         supportedLocales: AppLocalizations.supportedLocales,
         title: 'first demo',
         theme: ThemeData(
-            colorScheme: ColorScheme.fromSwatch().copyWith(
-          primary: const Color.fromARGB(197, 238, 255, 255),
-          secondary: const Color.fromRGBO(167, 252, 132, 0.7),
-        )),
-        home: TabsScreen(),
+          colorScheme: ColorScheme.fromSwatch().copyWith(
+            primary: const Color.fromARGB(197, 238, 255, 255),
+            secondary: const Color.fromRGBO(167, 252, 132, 0.7),
+          ),
+        ),
+        home: const LoginScreen(),
         routes: {
           SignInScreen.routeName: (ctx) => const SignInScreen(),
+          ShowInteractionsResultsScreen.routeName: (ctx) =>
+              const ShowInteractionsResultsScreen(),
           LoginScreen.routeName: (ctx) => const LoginScreen(),
           RegisterFormScreen.routeName: (ctx) => const RegisterFormScreen(),
           ExamsScreen.routeName: (ctx) => const ExamsScreen(),
