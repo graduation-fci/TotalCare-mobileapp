9a148ad8ba5397b4e798d036facf916199e5408b
hassan-prog <alielsayed1112@gmail.com>
2023-04-22 02:29:04 +0200

    minor code refactoring

--- grad_login/lib/providers/medicineProvider.dart
+++ grad_login/lib/providers/medicineProvider.dart
@@ -15,38 +15,17 @@ class MedicineProvider with ChangeNotifier {
   MedicineService medicineService = MedicineService();
   int currentPage = 1;
 
-  List<Exam> _exams = [];
-
-  List<Exam> get exams {
-    return [..._exams];
-  }
-
   Future<void> getMedicines() async {
     appState = AppState.loading;
     notifyListeners();
     final responseData = await medicineService.getSimpleMeds();
     if (responseData!['detail'] != null) {
       errorMessage = responseData['detail'];
-      // print(responseData['detail']);
       appState = AppState.error;
     } else {
       response = responseData;
-      List<Exam> loadedData = [];
-      // for (var medicineData in responseData['results']) {
-      //   loadedData.add(
-      //     Exam(
-      //       title: medicineData['title'],
-      //       id: medicineData['id'],
-      //       subject: medicineData['subject'],
-      //       startsAt: medicineData['starts_at'],
-      //       endsAt: medicineData['ends_at'],
-      //     ),
-      //   );
-      // }
       log("$response");
-      _exams = _exams + loadedData;
       currentPage += 1;
-      // log('$response');
       appState = AppState.done;
     }
     notifyListeners();
