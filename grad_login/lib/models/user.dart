c50b631c62ad6bf2f5472bd3fc7ed749122f4ef8
hassan-prog <alielsayed1112@gmail.com>
2023-04-25 09:00:33 +0200

    corrected variable names to match the expected ones in backend

--- grad_login/lib/models/user.dart
+++ grad_login/lib/models/user.dart
@@ -1,15 +1,17 @@
+// ignore_for_file: non_constant_identifier_names
+
 class User {
   String username;
-  String firstName;
-  String lastName;
+  String first_name;
+  String last_name;
   String email;
   String password;
   String profileType;
 
   User({
     required this.username,
-    required this.firstName,
-    required this.lastName,
+    required this.first_name,
+    required this.last_name,
     required this.email,
     required this.password,
     required this.profileType,
