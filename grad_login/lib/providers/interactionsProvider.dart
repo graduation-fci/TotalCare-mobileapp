dcbafe5f11f5cc0346fd788c1c739958e597f3d4
hassan-prog <alielsayed1112@gmail.com>
2023-04-25 06:21:36 +0200

    the response is now a List instead of just a single Map object

--- grad_login/lib/providers/interactionsProvider.dart
+++ grad_login/lib/providers/interactionsProvider.dart
@@ -1,30 +1,23 @@
-import 'dart:convert';
 import 'dart:developer';
-import 'package:http/http.dart' as http;
 import 'package:flutter/cupertino.dart';
 
 import '../infrastructure/medicine/interactions_service.dart';
-import '../models/drug.dart';
-import '../models/simple_medicine.dart';
 
 import '../app_state.dart';
-import '../models/exam.dart';
-import '../my_config.dart';
 
 class InteractionsProvider with ChangeNotifier {
   AppState appState = AppState.init;
   String? errorMessage;
   InteractionsService interactionsService = InteractionsService();
   int currentPage = 1;
-  Map<String, dynamic> _response = {};
+  List _response = [];
 
-  Map<String, dynamic> get response {
-    return {..._response};
+  List get response {
+    return [..._response];
   }
 
   Future<void> getInteractions(
       List<Map<String, dynamic>> interactionMeds) async {
-    // log('$interactionMeds');
     appState = AppState.loading;
     notifyListeners();
     final responseData =
@@ -35,9 +28,8 @@ class InteractionsProvider with ChangeNotifier {
     } else {
       appState = AppState.done;
     }
-    _response = responseData['permutations'][0];
+    _response = responseData['permutations'];
     log("$_response");
-    // log("$response");
     notifyListeners();
   }
 }
