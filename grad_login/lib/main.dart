8629abcc0554b7b0f2a8d07431a0ae2e60a9c601
hassan-prog <alielsayed1112@gmail.com>
2023-04-27 07:23:19 +0200

    created separate function of ThemeData

--- grad_login/lib/main.dart
+++ grad_login/lib/main.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:grad_login/providers/interactionsProvider.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:grad_login/providers/categories.dart';
 
 import 'screens/tabs_screen.dart';
 import 'screens/show_interactions_results_screen.dart';
 import 'screens/login_screen.dart';
 import 'screens/register_screen.dart';
-import 'screens/sign_screen.dart';
 import 'providers/medicineProvider.dart';
 import 'providers/userProvider.dart';
 import 'providers/authProvider.dart';
@@ -35,46 +35,64 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider.value(
           value: InteractionsProvider(),
         ),
+        ChangeNotifierProvider.value(
+          value: Categories(),
+        ),
       ],
       child: MaterialApp(
         localizationsDelegates: AppLocalizations.localizationsDelegates,
         supportedLocales: AppLocalizations.supportedLocales,
         title: 'first demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSwatch().copyWith(
-            primary: const Color(0xFFC5EEFF),
-            secondary: const Color.fromRGBO(167, 252, 132, 0.7),
-          ),
-          primaryTextTheme: TextTheme(
-            titleLarge: TextStyle(
-              color: Colors.grey.shade800,
-              fontWeight: FontWeight.w600,
-              fontSize: 32,
-            ),
-            titleMedium: TextStyle(
-              color: Colors.grey.shade800,
-              fontWeight: FontWeight.w600,
-              fontSize: 20,
-            ),
-            bodySmall: TextStyle(color: Colors.grey.shade800, fontSize: 12,)
-          ),
-          textTheme: const TextTheme(
-            button: TextStyle(
-              fontFamily: 'NotoSans',
-              fontSize: 14,
-              color: Color(0xFF003B4A),
-            ),
-          ),
-        ),
+        theme: _buildThemeData(),
         home: const LoginScreen(),
         routes: {
-          SignInScreen.routeName: (ctx) => const SignInScreen(),
           ShowInteractionsResultsScreen.routeName: (ctx) =>
               const ShowInteractionsResultsScreen(),
           LoginScreen.routeName: (ctx) => const LoginScreen(),
           RegisterFormScreen.routeName: (ctx) => const RegisterFormScreen(),
           TabsScreen.routeName: (ctx) => const TabsScreen(),
         },
+      ),
+    );
+  }
+
+  ThemeData _buildThemeData() {
+    return ThemeData(
+      colorScheme: ColorScheme.fromSwatch().copyWith(
+        primary: const Color(0xFFC5EEFF),
+        secondary: const Color(0xFF003745),
+      ),
+      primaryTextTheme: TextTheme(
+        titleLarge: TextStyle(
+          color: Colors.grey.shade800,
+          fontWeight: FontWeight.w600,
+          fontSize: 32,
+        ),
+        titleMedium: TextStyle(
+          color: Colors.grey.shade800,
+          fontWeight: FontWeight.w600,
+          fontSize: 20,
+        ),
+        bodySmall: TextStyle(
+          color: Colors.grey.shade800,
+          fontSize: 12,
+        ),
+      ),
+      textTheme: const TextTheme(
+        button: TextStyle(
+          fontFamily: 'NotoSans',
+          fontSize: 14,
+          color: Color(0xFF003B4A),
+        ),
+      ),
+      appBarTheme: const AppBarTheme(
+        titleTextStyle: TextStyle(
+          fontFamily: 'Anton-Regular',
+          color: Color(0xFF003745),
+          fontWeight: FontWeight.w700,
+          fontSize: 20,
+        ),
+        elevation: 1,
       ),
     );
   }
