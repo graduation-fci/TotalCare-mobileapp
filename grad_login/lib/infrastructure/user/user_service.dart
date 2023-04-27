a6f2a3753ef20b2963d15a9865877ff1d894efa3
hassan-prog <alielsayed1112@gmail.com>
2023-04-27 07:57:28 +0200

    created request to get user by id

--- grad_login/lib/infrastructure/user/user_service.dart
+++ grad_login/lib/infrastructure/user/user_service.dart
@@ -7,8 +7,28 @@ import '../../my_config.dart';
 import '../shared/storage.dart';
 
 class UserService {
-  var apiEndPoint = Config.simpleMeds;
   Storage storage = Storage();
 
-  
+  Future<Map<String, dynamic>> getUserById(int id) async {
+    final url = Uri.parse('${Config.getUserById}$id');
+    String? token;
+    await storage.getToken().then((value) {
+      token = value;
+    });
+
+    final response = await http.get(
+      url,
+      headers: {
+        "content-type": "application/json",
+        "Authorization": "JWT $token",
+      },
+    );
+
+    final responseData = json.decode(response.body);
+    log(responseData);
+    if (responseData['details'] == null) {
+      return responseData;
+    }
+    return responseData;
+  }
 }
