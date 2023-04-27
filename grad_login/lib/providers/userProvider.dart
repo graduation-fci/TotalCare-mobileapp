687656075f95f3419044f7249a76fa7a9087bb54
hassan-prog <alielsayed1112@gmail.com>
2023-04-27 08:40:46 +0200

    added function to get user's profile

--- grad_login/lib/providers/userProvider.dart
+++ grad_login/lib/providers/userProvider.dart
@@ -1,17 +1,34 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
+import '../app_state.dart';
 import '../infrastructure/user/user_service.dart';
 import '../models/user.dart';
 
 class UserProvider with ChangeNotifier {
   UserService userService = UserService();
+  String? errorMessage;
+  Map<String, dynamic>? response;
+  AppState appState = AppState.init;
   final List<User> _users = [];
 
   List<User> get users {
     return [..._users];
   }
 
-  Future<Map<String, dynamic>?> getUserById(int id) {
-    return userService.getUserById(id);
+  Future<void> getUserProfile() async {
+    appState = AppState.loading;
+    notifyListeners();
+    final responseData = await userService.getMyProfile();
+    if (responseData['detail'] != null) {
+      errorMessage = responseData['detail'];
+      appState = AppState.error;
+    } else {
+      appState = AppState.done;
+    }
+    response = responseData;
+    log("$response");
+    notifyListeners();
   }
 }
