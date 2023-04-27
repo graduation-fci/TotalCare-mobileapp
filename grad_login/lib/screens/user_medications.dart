dfb1e380b463b40451b42fa5ae3602add6f1acd6
hassan-prog <alielsayed1112@gmail.com>
2023-04-27 07:27:25 +0200

    first commit of UserMedicationsScreen

---
+++ grad_login/lib/screens/user_medications.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class UserMedicationsScreen extends StatefulWidget {
+  static const routeName = 'user-medications-screen';
+  const UserMedicationsScreen({super.key});
+
+  @override
+  State<UserMedicationsScreen> createState() => _UserMedicationsScreenState();
+}
+
+class _UserMedicationsScreenState extends State<UserMedicationsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
