0ff2dd9429c8f0d33ba3a3e126ec197b6fd2e50f
hassan-prog <alielsayed1112@gmail.com>
2023-04-28 07:53:57 +0300

    solved the problem of displaying medicine names&scroll

--- grad_login/lib/screens/user_medications.dart
+++ grad_login/lib/screens/user_medications.dart
@@ -1,9 +1,10 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
-import 'package:grad_login/app_state.dart';
-import 'package:grad_login/providers/userProvider.dart';
 import 'package:provider/provider.dart';
 
-import '../infrastructure/user/user_service.dart';
+import '../app_state.dart';
+import '../providers/userProvider.dart';
 
 class UserMedicationsScreen extends StatefulWidget {
   static const routeName = 'user-medications-screen';
@@ -16,6 +17,8 @@ class UserMedicationsScreen extends StatefulWidget {
 class _UserMedicationsScreenState extends State<UserMedicationsScreen> {
   List<Map<String, dynamic>> medication = [];
   AppState appState = AppState.init;
+  // Map medications = {};
+  Map<int, List> medications = {};
   @override
   void initState() {
     Future.delayed(Duration.zero).then((value) async {
@@ -31,15 +34,18 @@ class _UserMedicationsScreenState extends State<UserMedicationsScreen> {
     final userProvider = Provider.of<UserProvider>(context, listen: false);
     final userProfileData = userProvider.userProfileData;
     final userMedications = userProvider.userMedications;
-    // print(userMedications);
-    // for (int i = 0; i <= userMedications['count']; i++) {
-    //   for (int k = 0;
-    //       k <= userMedications['results'][i]['medicine'].length;
-    //       k++) {
-    //     medicationNames
-    //         .add(userMedications['results'][i]['medicine'][k]['name']);
-    //   }
-    // }
+    final medicationResults = userMedications['results'];
+
+    for (int i = 0; i < userMedications['count']; i++) {
+      List medicineNames = [];
+
+      for (int k = 0;
+          k < userMedications['results'][i]['medicine'].length;
+          k++) {
+        medicineNames.add(userMedications['results'][i]['medicine'][k]['name']);
+      }
+      medications[i] = medicineNames;
+    }
 
     return Scaffold(
       appBar: AppBar(
@@ -48,85 +54,108 @@ class _UserMedicationsScreenState extends State<UserMedicationsScreen> {
           style: Theme.of(context).appBarTheme.titleTextStyle,
         ),
       ),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            children: [
-              // ignore: avoid_unnecessary_containers
-              Container(
-                child: Image.asset(
-                  'assets/images/TotalCare.png',
-                  height: mediaQuery.height * 0.24,
-                  width: mediaQuery.width * 0.3,
-                ),
+      resizeToAvoidBottomInset: false,
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: constraints.maxHeight,
               ),
-              Text(
-                '${userProfileData['first_name']} ${userProfileData['last_name']}',
-                style: Theme.of(context).textTheme.titleMedium,
-              ),
-              Container(
-                height: mediaQuery.height,
-                padding: const EdgeInsets.all(12),
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  // physics: const NeverScrollableScrollPhysics(),
-                  itemBuilder: (context, index) {
-                    if (medication.length > 1) {
-                      medication.removeAt(index - 1);
-                    }
-                    for (int i = 0;
-                        i <
-                            userMedications['results'][index]['medicine']
-                                .length;
-                        i++) {
-                      medication.add(
-                          userMedications['results'][index]['medicine'][i]);
-                    }
-
-                    return Column(
+              child: Column(
+                children: [
+                  // ignore: avoid_unnecessary_containers
+                  Center(
+                    child: Column(
                       children: [
-                        userMedications['results'][index] == 0
-                            ? ListTile(
-                                title: Text(
-                                  '${userMedications['results'][index]['title']}',
-                                  style:
-                                      Theme.of(context).textTheme.headlineLarge,
-                                ),
-                                subtitle: Text(
-                                  medication.join(', '),
-                                ),
-                              )
-                            : const Divider(thickness: 0.8, color: Colors.grey),
-                        ListTile(
-                          title: Text(
-                            '${userMedications['results'][index]['title']}',
-                            style: Theme.of(context).textTheme.headlineLarge,
-                          ),
-                          subtitle: Text(
-                            medication.map((med) => med['name']).join(', '),
-                          ),
-                          trailing: IconButton(
-                            icon: const Icon(
-                              Icons.delete,
-                              color: Colors.red,
-                            ),
-                            onPressed: () {
-                              userProvider.delMedication(
-                                userMedications['results'][index]['id'],
-                              );
-                              setState(() {});
-                            },
-                          ),
+                        Image.asset(
+                          'assets/images/TotalCare.png',
+                          height: mediaQuery.height * 0.24,
+                          width: mediaQuery.width * 0.3,
+                        ),
+                        Text(
+                          '${userProfileData['first_name']} ${userProfileData['last_name']}',
+                          style: Theme.of(context).textTheme.titleMedium,
                         ),
                       ],
-                    );
-                  },
-                  itemCount: userMedications['results'].length,
-                ),
-              )
-            ],
-          ),
-        ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Container(
+                    padding: const EdgeInsets.all(12),
+                    child: ListView.builder(
+                      key: GlobalKey(),
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemBuilder: (context, index) {
+                        return Card(
+                          elevation: 5,
+                          child: Padding(
+                            padding: const EdgeInsets.all(6),
+                            child: ListTile(
+                              title: Text(
+                                '${medicationResults[index]['title']}',
+                                style:
+                                    Theme.of(context).textTheme.headlineLarge,
+                              ),
+                              subtitle: Text(
+                                medications[index]!.join(', '),
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                              trailing: SizedBox(
+                                width: 80,
+                                child: Row(
+                                  children: [
+                                    Expanded(
+                                      child: IconButton(
+                                        icon: const Icon(
+                                          Icons.delete,
+                                          color: Colors.red,
+                                        ),
+                                        onPressed: () async {
+                                          await userProvider.delMedication(
+                                            medicationResults[index]['id'],
+                                          );
+                                          setState(() {
+                                            medicationResults.remove(
+                                                medicationResults[index]);
+                                          });
+                                          log('$medicationResults');
+                                        },
+                                      ),
+                                    ),
+                                    Expanded(
+                                      child: IconButton(
+                                        icon: const Icon(
+                                          Icons.edit,
+                                          color: Colors.grey,
+                                        ),
+                                        onPressed: () {
+                                          userProvider.delMedication(
+                                            userMedications['results'][index]
+                                                ['id'],
+                                          );
+                                          // userProvider.getUserMedications();
+                                          setState(() {});
+                                        },
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                      itemCount: medicationResults.length,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        },
       ),
     );
   }
