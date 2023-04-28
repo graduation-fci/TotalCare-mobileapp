eb420c651988c795b10169a5b3b381e2993f10db
hassan-prog <alielsayed1112@gmail.com>
2023-04-28 19:25:26 +0300

    config file updated

--- grad_login/lib/my_config.dart
+++ grad_login/lib/my_config.dart
@@ -4,4 +4,6 @@ class Config {
   static const String interactionMain = '$apiUrl/medicine/interactions/';
   static const String login = '$apiUrl/auth/jwt/create/';
   static const String register = '$apiUrl/auth/users/';
+  static const String myProfile = '$apiUrl/auth/users/me/';
+  static const String userMedications = '$apiUrl/users/medications/';
 }
