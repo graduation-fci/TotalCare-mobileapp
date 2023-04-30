f07466bec5e871a4c5a56506f39c137f961d2094
hassan-prog <alielsayed1112@gmail.com>
2023-05-01 00:22:26 +0300

    add if statements to check if either values is empty for patch request

--- grad_login/lib/infrastructure/user/user_service.dart
+++ grad_login/lib/infrastructure/user/user_service.dart
@@ -60,20 +60,28 @@ class UserService {
   Future<Map<String, dynamic>> editMedicationProfile(
       Medication med, int id) async {
     final url = Uri.parse('${Config.userMedications}$id/');
+    Map<String, dynamic> body = {};
+    if (med.title.isNotEmpty && med.medicines.isNotEmpty) {
+      body = {"title": med.title, "medicines": med.medicines};
+    } else if (med.title.isEmpty && med.medicines.isNotEmpty) {
+      body = {"medicines": med.medicines};
+    } else if (med.medicines.isEmpty && med.title.isNotEmpty) {
+      body = {"title": med.title};
+    }
+
     String? token;
     await storage.getToken().then((value) {
       token = value;
     });
 
-    final response = await http.patch(url,
-        headers: {
-          "content-type": "application/json",
-          "Authorization": "JWT $token",
-        },
-        body: json.encode({
-          "title": med.title,
-          "medicines": med.medicines,
-        }));
+    final response = await http.patch(
+      url,
+      headers: {
+        "content-type": "application/json",
+        "Authorization": "JWT $token",
+      },
+      body: json.encode(body),
+    );
 
     final responseData = json.decode(response.body);
     // log('$responseData');
