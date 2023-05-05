7723c7eb9ca36aac5c80f5440744faef2acd4dc5
hassan-prog <alielsayed1112@gmail.com>
2023-05-05 07:11:01 +0300

    created searchCategories request and provider func

--- grad_login/lib/infrastructure/medicine/medicine_service.dart
+++ grad_login/lib/infrastructure/medicine/medicine_service.dart
@@ -59,6 +59,32 @@ class MedicineService {
     return responseData;
   }
 
+  Future<Map<String, dynamic>?> filterCategories({String? search}) async {
+    const baseUrl = Config.categories;
+    final queryParams = <String, String>{};
+    String? token;
+    await storage.getToken().then((value) {
+      token = value;
+    });
+    if (search != null) {
+      queryParams['search'] = search;
+    }
+
+    final url = Uri.parse('$baseUrl?${_getQueryString(queryParams)}');
+    final response = await http.get(
+      url,
+      headers: {
+        "content-type": "application/json",
+        "Authorization": "JWT $token",
+      },
+    );
+    final responseData = json.decode(response.body);
+    if (responseData['details'] == null) {
+      return responseData;
+    }
+    return responseData;
+  }
+
   String _getQueryString(Map<String, String> params) {
     return params.entries
         .map((e) => '${e.key}=${Uri.encodeQueryComponent(e.value)}')
