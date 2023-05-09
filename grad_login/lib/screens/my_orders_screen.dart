4c372f2be1c1dfbb317f163d3e19a651fad89e3c
hassan-prog <alielsayed1112@gmail.com>
2023-05-09 05:48:06 +0300

    error handling & called getOrders func in initState

--- grad_login/lib/screens/my_orders_screen.dart
+++ grad_login/lib/screens/my_orders_screen.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:grad_login/app_state.dart';
+import 'package:grad_login/providers/orders_provider.dart';
+import 'package:provider/provider.dart';
 
 import '../widgets/order_item.dart';
 
 class MyOrdersScreen extends StatefulWidget {
+  static const String routeName = 'my-orders-screen';
   const MyOrdersScreen({super.key});
 
   @override
@@ -11,21 +15,37 @@ class MyOrdersScreen extends StatefulWidget {
 
 class _MyOrdersScreenState extends State<MyOrdersScreen> {
   int _selectedPageIndex = 0;
+
+  @override
+  void initState() {
+    Future.delayed(Duration.zero).then(
+        (_) => Provider.of<OrdersProvider>(context, listen: false).getOrders());
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final ordersProvider = Provider.of<OrdersProvider>(context);
+    final ordersData = ordersProvider.getOrdersData;
     final mediaQuery = MediaQuery.of(context).size;
+
     return SafeArea(
       child: Scaffold(
         body: SingleChildScrollView(
           child: Column(
+            mainAxisAlignment: ordersProvider.appState == AppState.error
+                ? MainAxisAlignment.center
+                : MainAxisAlignment.start,
             children: [
               SizedBox(
                 height: 110,
+                width: double.infinity,
                 child: CustomScrollView(
                   physics: const NeverScrollableScrollPhysics(),
                   shrinkWrap: true,
                   slivers: [
                     SliverAppBar(
+                      centerTitle: true,
                       backgroundColor: Colors.white,
                       pinned: true,
                       elevation: 0,
@@ -44,20 +64,28 @@ class _MyOrdersScreenState extends State<MyOrdersScreen> {
                         ),
                       ),
                     ),
-                    SliverAppBar(
-                      primary: false,
-                      pinned: true,
-                      backgroundColor: Colors.white,
-                      elevation: 0,
-                      title: topNavBar(),
-                    ),
                   ],
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.all(20),
-                child: orderItem(),
-              ),
+              ordersProvider.appState == AppState.error
+                  ? Center(
+                      child: Text(
+                        '${ordersProvider.errorMsg}',
+                        style: const TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                    )
+                  : Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: ListView.builder(
+                        physics: const NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        itemBuilder: (context, index) {
+                          return orderItem(ordersData, index);
+                        },
+                        itemCount: ordersData.length,
+                      ),
+                    ),
             ],
           ),
         ),
