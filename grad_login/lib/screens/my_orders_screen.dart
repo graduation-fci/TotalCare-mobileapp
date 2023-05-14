0da44f51811a8873772357b1b1f163a0e0f4f6a5
hassan-prog <alielsayed1112@gmail.com>
2023-05-14 13:05:01 +0300

    fixed appbar height and color

--- grad_login/lib/screens/my_orders_screen.dart
+++ grad_login/lib/screens/my_orders_screen.dart
@@ -38,7 +38,7 @@ class _MyOrdersScreenState extends State<MyOrdersScreen> {
                 : MainAxisAlignment.start,
             children: [
               SizedBox(
-                height: 110,
+                height: 60,
                 width: double.infinity,
                 child: CustomScrollView(
                   physics: const NeverScrollableScrollPhysics(),
@@ -46,7 +46,7 @@ class _MyOrdersScreenState extends State<MyOrdersScreen> {
                   slivers: [
                     SliverAppBar(
                       centerTitle: true,
-                      backgroundColor: Colors.white,
+                      backgroundColor: Colors.transparent,
                       pinned: true,
                       elevation: 0,
                       leading: IconButton(
@@ -88,7 +88,7 @@ class _MyOrdersScreenState extends State<MyOrdersScreen> {
                         physics: const NeverScrollableScrollPhysics(),
                         shrinkWrap: true,
                         itemBuilder: (context, index) {
-                          return orderItem(context ,ordersData, index);
+                          return orderItem(context, ordersData, index);
                         },
                         itemCount: ordersData.length,
                       ),
