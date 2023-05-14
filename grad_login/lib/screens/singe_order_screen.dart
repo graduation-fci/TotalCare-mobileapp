77a3893610fcff0d47751dc3c9e168ab1eb9a426
hassan-prog <alielsayed1112@gmail.com>
2023-05-14 11:49:37 +0300

    first commit of SingleOrderScreen

---
+++ grad_login/lib/screens/singe_order_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SingleOrderScreen extends StatefulWidget {
+  static const String routeName = 'single-order-screen';
+  const SingleOrderScreen({super.key});
+
+  @override
+  State<SingleOrderScreen> createState() => _SingleOrderScreenState();
+}
+
+class _SingleOrderScreenState extends State<SingleOrderScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
