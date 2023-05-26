fa4867c15ddfaf7459fa16631d561002afe13233
hassan-prog <alielsayed1112@gmail.com>
2023-05-26 03:44:06 +0300

    added functionality to the widget

--- grad_login/lib/widgets/mobile_number_field.dart
+++ grad_login/lib/widgets/mobile_number_field.dart
@@ -1,19 +1,32 @@
+import 'dart:developer';
+
 import 'package:country_picker/country_picker.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-
 import 'package:flutter/material.dart';
 
+import '../models/user.dart';
+
 class MobileNumberField extends StatefulWidget {
-  const MobileNumberField({super.key});
+  final TextEditingController countryController;
+  final TextEditingController phoneController;
+  final String? Function(String?) validator;
+  User? user;
+
+  MobileNumberField({
+    super.key,
+    required this.countryController,
+    required this.phoneController,
+    required this.validator,
+    this.user,
+  });
 
   @override
   State<MobileNumberField> createState() => _MobileNumberFieldState();
 }
 
 class _MobileNumberFieldState extends State<MobileNumberField> {
-  final TextEditingController countryController = TextEditingController();
-  final TextEditingController phoneController = TextEditingController();
   final FocusNode countryFocus = FocusNode();
+
   @override
   Widget build(BuildContext context) {
     final appLocalization = AppLocalizations.of(context)!;
@@ -21,9 +34,11 @@ class _MobileNumberFieldState extends State<MobileNumberField> {
     String countryName = appLocalization.countryName;
     Color containerFillColor = Colors.grey.shade100;
     Color labelColor = Colors.grey;
+    widget.countryController.text = '+20';
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 10),
       child: Container(
+        width: double.infinity,
         decoration: BoxDecoration(
           boxShadow: [
             BoxShadow(
@@ -48,7 +63,7 @@ class _MobileNumberFieldState extends State<MobileNumberField> {
                 showPhoneCode: true,
                 onSelect: (Country country) {
                   country.flagEmoji;
-                  countryController.text = '+${country.phoneCode}';
+                  widget.countryController.text = '+${country.phoneCode}';
                   countryName = '${country.flagEmoji}  ${country.name}';
                   setState(() {});
                 },
@@ -58,24 +73,23 @@ class _MobileNumberFieldState extends State<MobileNumberField> {
                 color: Colors.grey,
               ),
             ),
-            countryController.text.isEmpty
-                ? const Text(
-                    '---',
-                    style: TextStyle(color: Colors.grey),
-                  )
-                : TextFormField(
-                    controller: countryController,
-                    decoration: const InputDecoration(
-                      border: InputBorder.none,
-                    ),
-                    enabled: false,
-                  ),
+            SizedBox(
+              width: 30,
+              child: TextFormField(
+                controller: widget.countryController,
+                decoration: const InputDecoration(
+                  border: InputBorder.none,
+                ),
+                enabled: false,
+              ),
+            ),
             Expanded(
               child: TextFormField(
-                controller: phoneController,
+                controller: widget.phoneController,
                 textInputAction: TextInputAction.next,
                 keyboardType: TextInputType.phone,
                 cursorColor: labelColor,
+                validator: widget.validator,
                 decoration: InputDecoration(
                   contentPadding: const EdgeInsets.fromLTRB(20, 5, 5, 5),
                   labelText: appLocalization.phoneNumber,
