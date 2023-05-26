ad3754a9a936e76019b0f6a3b05677a6bee47f6c
hassan-prog <alielsayed1112@gmail.com>
2023-05-26 03:03:40 +0300

    added backBtn and fixed general styling

--- grad_login/lib/screens/add_address_screen.dart
+++ grad_login/lib/screens/add_address_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:grad_login/providers/addressProvider.dart';
 import 'package:provider/provider.dart';
-import 'package:country_picker/country_picker.dart';
+
+import '../providers/addressProvider.dart';
+import '../widgets/mobile_number_field.dart';
 
 class AddAddressScreen extends StatefulWidget {
   const AddAddressScreen({Key? key}) : super(key: key);
@@ -19,8 +20,9 @@ class _AddAddressScreenState extends State<AddAddressScreen> {
   final _cityController = TextEditingController();
   final _descController = TextEditingController();
   final _phoneController = TextEditingController();
-  AddressType _addressType = AddressType.Home;
+  final _countryController = TextEditingController();
   final _titleController = TextEditingController();
+  AddressType _addressType = AddressType.Home;
 
   String countryName = 'Choose Country';
 
@@ -36,172 +38,220 @@ class _AddAddressScreenState extends State<AddAddressScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Color(0xFF003745),
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
         centerTitle: true,
-        title: const Text('Add Address'),
+        title: Text(
+          'Add Address'.toUpperCase(),
+          style: Theme.of(context)
+              .appBarTheme
+              .titleTextStyle!
+              .copyWith(fontSize: mediaQuery.width * 0.045),
+        ),
       ),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Form(
-                key: _formKey,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                TextFormField(
+                  cursorColor: Colors.grey,
+                  controller: _streetController,
+                  decoration: InputDecoration(
+                    labelText: 'Street Address',
+                    labelStyle: const TextStyle(
+                      color: Colors.grey,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(color: Colors.grey),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter a street address';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 16.0),
+                TextFormField(
+                  cursorColor: Colors.grey,
+                  controller: _cityController,
+                  decoration: InputDecoration(
+                    labelStyle: const TextStyle(
+                      color: Colors.grey,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(color: Colors.grey),
+                    ),
+                    labelText: 'City',
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter a city';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 16.0),
+                TextFormField(
+                  cursorColor: Colors.grey,
+                  controller: _descController,
+                  maxLines: 3,
+                  decoration: InputDecoration(
+                    labelText: 'Description',
+                    labelStyle: const TextStyle(
+                      color: Colors.grey,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(color: Colors.grey),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter a description';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 16.0),
+                TextFormField(
+                  cursorColor: Colors.grey,
+                  controller: _titleController,
+                  decoration: InputDecoration(
+                    labelText: 'Title',
+                    labelStyle: const TextStyle(
+                      color: Colors.grey,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(color: Colors.grey),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter a valid Title';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 16.0),
+                MobileNumberField(
+                  countryController: _countryController,
+                  phoneController: _phoneController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Phone Number cannot be empty';
+                    }
+                  },
+                ),
+                const SizedBox(height: 16.0),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    TextFormField(
-                      controller: _streetController,
-                      decoration: const InputDecoration(
-                        labelText: 'Street Address',
-                        border: OutlineInputBorder(),
-                      ),
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return 'Please enter a street address';
-                        }
-                        return null;
-                      },
+                    const Text(
+                      'Address Type',
+                      style: TextStyle(fontSize: 16),
                     ),
-                    const SizedBox(height: 16.0),
-                    TextFormField(
-                      controller: _cityController,
-                      decoration: const InputDecoration(
-                        labelText: 'City',
-                        border: OutlineInputBorder(),
-                      ),
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return 'Please enter a city';
-                        }
-                        return null;
-                      },
-                    ),
-                    const SizedBox(height: 16.0),
-                    TextFormField(
-                      controller: _descController,
-                      maxLines: 3,
-                      decoration: const InputDecoration(
-                        labelText: 'Description',
-                        border: OutlineInputBorder(),
-                      ),
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return 'Please enter a description';
-                        }
-                        return null;
-                      },
-                    ),
-                    const SizedBox(height: 16.0),
-                    TextFormField(
-                      controller: _titleController,
-                      decoration: const InputDecoration(
-                        labelText: 'Title',
-                        border: OutlineInputBorder(),
-                      ),
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return 'Please enter a valid Title';
-                        }
-                        return null;
-                      },
-                    ),
-                    const SizedBox(height: 16.0),
-                    SizedBox(
-                      width: double.infinity,
-                      child: Row(
-                        children: [
-                          TextButton.icon(
-                            label: Text(countryName),
-                            onPressed: () => showCountryPicker(
-                              context: context,
-                              showPhoneCode: true,
-                              onSelect: (Country country) {
-                                country.flagEmoji;
-                                _phoneController.text = '+${country.phoneCode}';
-                                countryName =
-                                    '${country.flagEmoji}  ${country.name}';
-                                setState(() {});
-                              },
-                            ),
-                            icon: const Icon(Icons.arrow_drop_down),
-                          ),
-                          Expanded(
-                            child: TextFormField(
-                              controller: _phoneController,
-                              keyboardType: TextInputType.phone,
-                              onSaved: (value) {
-                                // _authData!. = value;
-                              },
-                              validator: (value) {
-                                if (value == null || value.isEmpty) {
-                                  return 'Phone Number cannot be empty';
-                                }
-                              },
-                              decoration: const InputDecoration(
-                                prefixIcon: Icon(Icons.phone),
-                                contentPadding: EdgeInsets.all(5),
-                                border: OutlineInputBorder(),
-                                labelText: 'Phone Number ',
-
-                                // focusedBorder: OutlineInputBorder(),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    const SizedBox(height: 16.0),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                    Row(
                       children: [
-                        const Text(
-                          'Address Type',
-                          style: TextStyle(fontSize: 16),
+                        Radio<AddressType>(
+                          value: AddressType.Home,
+                          groupValue: _addressType,
+                          onChanged: (AddressType? value) {
+                            setState(() {
+                              _addressType = value!;
+                            });
+                          },
                         ),
-                        Row(
-                          children: [
-                            Radio<AddressType>(
-                              value: AddressType.Home,
-                              groupValue: _addressType,
-                              onChanged: (AddressType? value) {
-                                setState(() {
-                                  _addressType = value!;
-                                });
-                              },
-                            ),
-                            const Text('Home'),
-                            Radio<AddressType>(
-                              value: AddressType.Business,
-                              groupValue: _addressType,
-                              onChanged: (AddressType? value) {
-                                setState(() {
-                                  _addressType = value!;
-                                });
-                              },
-                            ),
-                            const Text('Business'),
-                          ],
+                        const Text('Home'),
+                        Radio<AddressType>(
+                          value: AddressType.Business,
+                          groupValue: _addressType,
+                          onChanged: (AddressType? value) {
+                            setState(() {
+                              _addressType = value!;
+                            });
+                          },
                         ),
+                        const Text('Business'),
                       ],
-                    ),
-                    const SizedBox(height: 16.0),
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: const Text('Select Location on Map'),
-                    ),
-                    const SizedBox(height: 16.0),
-                    ElevatedButton(
-                      onPressed: _submitForm,
-                      child: const Text('Save'),
                     ),
                   ],
                 ),
-              ),
-              const SizedBox(height: 16.0),
-            ],
+                const SizedBox(height: 16.0),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Theme.of(context).colorScheme.primary,
+                    fixedSize: Size(
+                      mediaQuery.width * 0.85,
+                      mediaQuery.height * 0.06,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(40),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    'Select Location on Map',
+                    style: Theme.of(context)
+                        .textTheme
+                        .button!
+                        .copyWith(fontSize: mediaQuery.width * 0.038),
+                  ),
+                ),
+                const SizedBox(height: 16.0),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Theme.of(context).colorScheme.primary,
+                    fixedSize: Size(
+                      mediaQuery.width * 0.85,
+                      mediaQuery.height * 0.06,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(40),
+                    ),
+                  ),
+                  onPressed: _submitForm,
+                  child: Text(
+                    'Save',
+                    style: Theme.of(context)
+                        .textTheme
+                        .button!
+                        .copyWith(fontSize: mediaQuery.width * 0.038),
+                  ),
+                ),
+                const SizedBox(height: 16.0),
+              ],
+            ),
           ),
         ),
       ),
