1b1b64c6d6120137872fe1ae1eee3c0ee0c4fb08
hassan-prog <alielsayed1112@gmail.com>
2023-05-28 01:59:12 +0300

    created showCustomDialog method

---
+++ lib/widgets/show_custom_dialog.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+Future<dynamic> showCustomDialog(
+  BuildContext context,
+  Widget titleWidget,
+  Widget? contentWidget,
+  List<Widget>? actionsWidgets,
+) {
+  return showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        title: titleWidget,
+        content: contentWidget,
+        actions: actionsWidgets,
+      );
+    },
+  );
+}
