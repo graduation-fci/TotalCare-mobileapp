bec9bb28fe284ee1ecff9443078b671d56679ab6
hassan-prog <alielsayed1112@gmail.com>
2023-06-16 22:34:42 +0300

    added refresh token url + notification

--- lib/my_config.dart
+++ lib/my_config.dart
@@ -1,7 +1,8 @@
 class Config {
-  // static const String apiUrl = "http://192.168.1.6:8000";
+  // static const String apiUrl = "http://192.168.1.8:8000";
   static const String apiUrl = "http://64.225.110.140:8000";
   static const String simpleMeds = '$apiUrl/medicine/simple_meds/';
+  static const String refreshToken = '$apiUrl/auth/auth/jwt/refresh/';
   static const String interactionMain = '$apiUrl/medicine/interactions/';
   static const String login = '$apiUrl/auth/jwt/create/';
   static const String register = '$apiUrl/auth/users/';
@@ -10,11 +11,11 @@ class Config {
   static const String generalCategories =
       '$apiUrl/medicine/general_categories/';
   static const String subCategories = '$apiUrl/medicine/categories/';
-  static const String refreshJWT = '$apiUrl/auth/jwt/refresh/';
   static const String drugs = '$apiUrl/medicine/products/';
   static const String addresses = '$apiUrl/users/addresses/';
   static const String carts = '$apiUrl/store/carts/';
   static const String orders = '$apiUrl/store/orders/';
   static const String patientProfile = '$apiUrl/users/patients/me/';
   static const String userImage = '$apiUrl/users/usersimages/';
+  static const String interactionNotification = '$apiUrl/medicine/profile-interactions/';
 }
